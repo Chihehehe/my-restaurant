e459db5929d86e2310badf9a119434fd642f5936
Chihehehe <quynhchipham1801@gmail.com>
2024-04-30 19:26:10 +1000

    edit index.js in backend folder

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,27 @@
 import express from "express"
+import mysql from "mysql"
 
 const app = express()
 
-app.listen(3000, () => {
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password:"password",
+    database: "myrestaurant"
+})
+
+app.get("/", (req,res) => {
+    res.json("hello this is the backend")
+})
+
+app.get("/editmenu", (req, res) => {
+    const q = "SELECT * FROM menu"
+    db.query(q, (err, data) => {
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+})
+
+app.listen(8800, () => {
     console.log("Connect to backend!")
 })
